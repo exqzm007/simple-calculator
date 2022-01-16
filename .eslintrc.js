c321f8b57208d3a95912e279e61372ef7542c9b5
vladyslav_ol <olieshchuk.v@brights.cloud>
2022-01-16 15:13:44 +0200

    Basic configuration

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
